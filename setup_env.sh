669f4dc8b0f2f5c472df107d8923f21c848af654
Aaron Gallagher <aaron.b.gallagher@gmail.com>
2016-10-06 10:52:33 -0700

    Remove the Linux kernel from the repo Instead, automatically clone the raspberry pi kernel when executing setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -2,6 +2,15 @@
 OLD_PWD=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 
+# clone the Raspberry Pi kernel and apply patches
+if [ ! -d "$(pwd)/kernel" ]; then
+ git clone --depth 1 https://github.com/raspberrypi/linux -b rpi-4.4.y "$(pwd)/kernel"
+fi
+cd "$(pwd)/kernel"
+git rev-parse HEAD > ../kernel-at-commit
+patch -p1 --no-backup-if-mismatch < ../kernel_patches/channel_switching_monitor_mode.patch
+cd ..
+
 if [ -d "$(pwd)/buildtools" ] && [ -d "$(pwd)/firmware_patching" ]; then
  export CC=$(pwd)/buildtools/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-
  export CROSS_COMPILE=$(pwd)/buildtools/arm-eabi-4.7/bin/arm-eabi-
